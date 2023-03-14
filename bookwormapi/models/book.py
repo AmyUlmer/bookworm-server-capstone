6f8f8e3c0f8ebb8e64fe1f2da75b2b7a498fb3ca
Amy Ulmer <aulmer703@gmail.com>
2023-03-13 21:27:47 -0500

    migrations completed

--- bookwormapi/models/book.py
+++ bookwormapi/models/book.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Book(models.Model):
-    author = models.CharField(on_delete=models.CASCADE,  max_length=50)
-    title = models.CharField(on_delete=models.CASCADE,  max_length=50)
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
     released_date = models.DateField()
     length = models.IntegerField()
     description = models.CharField(max_length=150)
-    book_genre = models.ForeignKey("Genre", on_delete=models.CASCADE)
+    book_genre = models.ForeignKey("BookGenre", on_delete=models.CASCADE)
+    reader = models.ForeignKey("Reader", on_delete=models.CASCADE)
