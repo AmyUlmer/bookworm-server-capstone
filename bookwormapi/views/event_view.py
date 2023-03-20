e0b7450ffd007c5d85b153be82e10bf937c57187
Amy Ulmer <aulmer703@gmail.com>
2023-03-20 14:45:32 -0500

    book and event views complete, added event_reader_view

--- bookwormapi/views/event_view.py
+++ bookwormapi/views/event_view.py
@@ -2,7 +2,8 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from bookwormapi.models import Event, Reader
+from bookwormapi.models import Event, Reader, Book
+from rest_framework.decorators import action
 
 
 class EventView(ViewSet):
@@ -20,35 +21,109 @@ class EventView(ViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def list(self, request):
-        """Handle GET requests to get all game types
+        """Handle GET requests to get all events
 
         Returns:
-            Response -- JSON serialized list of game types
+            Response -- JSON serialized list of events
         """
-        
-        events = Event.objects.all()
-        
-        #if reader parameter is included in, the events are filtered by 'reader_id' to only include events that that reader joined. 
-        if "reader" in request.query_params:
-            query = request.GET.get('reader')
-            query_int = int(query)
-            events = Event.objects.all()
-            events = events.filter(reader_id=query_int)
+        #get the book from the request query params
+        book = request.query_params.get('book')
+
+        if book:
+            #filter the event queryset by the Book id
+            events = Event.objects.filter(book_id=book)
         else:
+            #get all events
             events = Event.objects.all()
-
-        reader = Reader.objects.get(user=request.auth.user)
-
+        
         # for each event, function checks if the currently authenticated user is in the "attendees" list for event.
+        reader = Reader.objects.get(user=request.auth.user)
         # If they are, the 'joined' property on the event is set to 'true', otherwise 'false' 
         # Set the `joined` property on every event
         for event in events:
             # Check to see if the reader is in the attendees list on the event
             event.joined = reader in event.attendees.all()
-
-        # 'events' query is passed to the EventSerializer class to be serialized into JSON data
+    
         serializer = EventSerializer(events, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data)
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        #getting the user that is logged in 
+        organizing_reader = Reader.objects.get(user=request.auth.user)
+        #retrieve book from database. make sure the book the user is trying to add with new event actually exists in database  
+        book = Book.objects.get(pk=request.data["book"])
+
+        #whichever keys are used on the request.data must match what the client is passing to the server.
+        event = Book.objects.create(
+            event_name=request.data["event_name"],
+            location=request.data["location"],
+            date_of_event=request.data["date_of_event"],
+            start_time=request.data["start_time"],
+            end_time=request.data["start_time"],
+            max_capacity=request.data["max_capacity"],
+            image_url=request.data["image_url"],
+            organizing_reader=organizing_reader,
+            book=book
+        )
+        
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+    def update(self, request, pk):
+        """Handle PUT requests for a event
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        event = Event.objects.get(pk=pk)
+        event.event_name=request.data["event_name"]
+        event.location=request.data["location"],
+        event.date_of_event=request.data["date_of_event"],
+        event.start_time=request.data["start_time"],
+        event.end_time=request.data["start_time"],
+        event.max_capacity=request.data["max_capacity"],
+        event.image_url=request.data["image_url"]
+
+        book = Book.objects.get(pk=request.data["book"])
+        event.book = book
+        event.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        event = Event.objects.get(pk=pk)
+        event.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    @action(methods=['post'], detail=True)
+    def signup(self, request, pk):
+        """Post request for a user to sign up for an event"""
+
+        reader = Reader.objects.get(user=request.auth.user)
+        event = Event.objects.get(pk=pk)
+        event.attendees.add(reader)
+        return Response({'message': 'Reader added'}, status=status.HTTP_201_CREATED)
+    
+    @action(methods=['delete'], detail=True)
+    def leave(self, request, pk):
+        """Deletes request for a user to leave an event"""
+
+        reader = Reader.objects.get(user=request.auth.user)
+        event = Event.objects.get(pk=pk)
+        event.attendees.remove(reader)
+        return Response({'message': 'Reader removed'}, status=status.HTTP_204_NO_CONTENT)
+
+class EventBookSerializer(serializers.ModelSerializer):
+    """JSON serializer for games
+    """
+    class Meta:
+        model = Book
+        fields = ('id', 'title',)
 
 class OrganizerSerializer(serializers.ModelSerializer):
     """JSON serializer for organizers
@@ -66,11 +141,11 @@ class AttendeeSerializer(serializers.ModelSerializer):
 
 class EventSerializer(serializers.ModelSerializer):
     """JSON serializer for events"""
-    # game = EventGameSerializer(many=False)
-    reader = OrganizerSerializer(many=False)
+    book = EventBookSerializer(many=False)
+    organizing_reader = OrganizerSerializer(many=False)
     attendees = AttendeeSerializer(many=True)
 
     class Meta:
         model = Event
-        fields = ('id', 'event_name', 'book', 'organizing_reader', 'location',
+        fields = ('id', 'event_name', 'book', 'location', 'organizing_reader',
                 'date_of_event', 'start_time', 'end_time', 'max_capacity', 'image_url','attendees', 'joined')
